cf1e77709952b7e88aa27123a43181de1118eee6
Tim Escondo <tim.escondo@gmail.com>
2020-10-15 20:07:17 -0400

    1

---
+++ src/components/Employee.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class = 'info'>
+      My name is {{name}} and I am a {{job}}.
+  </div>    
+</template>
+
+<script>
+export default {
+    name: 'employee',
+    data() {
+        return {
+            name: 'Kaiba',
+            job: 'Duelist'
+        }
+    }
+}
+</script>
+
+<style>
+    .info {
+        color : red;
+    }
+</style>
